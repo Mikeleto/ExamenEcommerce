1f517fe8fc0e6d5651f455d031daa51d41942040
Carlos Abrisqueta <cmabris@gmail.com>
2024-01-16 19:30:33 +0000

    Paso 11

--- database/migrations/2024_01_16_184547_create_products_table.php
+++ database/migrations/2024_01_16_184547_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -30,6 +31,8 @@ class CreateProductsTable extends Migration
                 ->on('brands')->onDelete('cascade');
 
             $table->integer('quantity')->nullable();
+
+            $table->enum('status', [Product::BORRADOR, Product::PUBLICADO])->default(Product::BORRADOR);
 
             $table->timestamps();
         });
