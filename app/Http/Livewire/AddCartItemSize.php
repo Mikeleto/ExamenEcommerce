fa4ec14b3cac6ffe53fb703964d94245ae0391e3
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-01-25 19:52:24 +0100

    Fin vídeo 54

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -11,16 +11,35 @@ class AddCartItemSize extends Component
     public $sizes;
     public $size_id = '';
     public $colors = [];
+    public $qty = 1;
+    public $quantity = 0;
+    public $color_id = '';
 
     public function mount()
     {
         $this->sizes = $this->product->sizes;
     }
 
+    public function decrement()
+    {
+        $this->qty--;
+    }
+
+    public function increment()
+    {
+        $this->qty++;
+    }
+
     public function updatedSizeId($value)
     {
         $size = Size::find($value);
         $this->colors = $size->colors;
+    }
+
+    public function updatedColorId($value)
+    {
+        $size = Size::find($this->size_id);
+        $this->quantity = $size->colors->find($value)->pivot->quantity;
     }
 
     public function render()
