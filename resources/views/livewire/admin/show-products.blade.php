5dba06568e8a13dda537fbc78553cb7e2dd25148
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-02-06 21:11:43 +0100

    Fin vídeo 122

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -4,4 +4,39 @@
             Lista de productos
         </h2>
     </x-slot>
+
+    <div class="container-menu py-12">
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <x-table-responsive>
+                            <div class="px-6 py-4">
+                                <x-jet-input class="w-full"
+                                             wire:model="search"
+                                             type="text"
+                                             placeholder="Introduzca el nombre del producto a buscar" />
+                            </div>
+
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
+                                    …
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach($products as $product)
+                                    <tr>
+                                        ...
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <div class="px-6 py-4">
+                                {{ $products->links() }}
+                            </div>
+                        </x-table-responsive>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
