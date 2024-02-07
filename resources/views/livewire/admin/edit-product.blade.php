a75a754af706f3a546f82d2790bc611f34aff01a
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-02-07 19:40:13 +0100

    Fin vídeo 133

--- resources/views/livewire/admin/edit-product.blade.php
+++ resources/views/livewire/admin/edit-product.blade.php
@@ -55,7 +55,7 @@
                       x-init="ClassicEditor.create($refs.miEditor)
                         .then(function(editor){
                             editor.model.document.on('change:data', () => {
-                                @this.set('description', editor.getData())
+                                @this.set('product.description', editor.getData())
                             })
                         })
                         .catch( error => {
@@ -89,7 +89,7 @@
         </div>
     </div>
 
-    @if (!$this->subcategory->color && !$this->subcategory->size)
+    @if ($this->subcategory && !$this->subcategory->color && !$this->subcategory->size)
         <div>
             <x-jet-label value="Cantidad" />
             <x-jet-input
