1a4e448526fb70877bdb2f65355abebf82b93f60
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-02-08 17:06:43 +0100

    Fin vídeo 159

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -3,10 +3,38 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateCategory extends Component
 {
+    use WithFileUploads;
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+
     public $brands;
 
     public function mount()
@@ -19,9 +47,14 @@ class CreateCategory extends Component
         $this->brands = Brand::all();
     }
 
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
     public function save()
     {
-
+        $this->validate();
     }
 
     public function render()
--- resources/views/livewire/admin/create-category.blade.php
+++ resources/views/livewire/admin/create-category.blade.php
@@ -13,21 +13,25 @@
                 <x-jet-label>
                     Nombre
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input wire:model="createForm.name" type="text" class="w-full mt-1" />
+
+                <x-jet-input-error for="createForm.name" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
                     Slug
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input disabled wire:model="createForm.slug" type="text" class="w-full mt-1 bg-gray-100" />
+                <x-jet-input-error for="createForm.slug" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
                     Icono
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input wire:model.defer="createForm.icon" type="text" class="w-full mt-1" />
+                <x-jet-input-error for="createForm.icon" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
@@ -39,12 +43,16 @@
                     @foreach ($brands as $brand)
 
                         <x-jet-label>
-                            <x-jet-checkbox />
+                            <x-jet-checkbox
+                                wire:model.defer="createForm.brands"
+                                name="brands[]"
+                                value="{{$brand->id}}" />
                             {{$brand->name}}
                         </x-jet-label>
 
                     @endforeach
                 </div>
+                <x-jet-input-error for="createForm.brands" />
             </div>
 
             <div class="col-span-6 sm:col-span-4">
@@ -52,13 +60,18 @@
                     Imagen
                 </x-jet-label>
 
-                <input type="file" class="mt-1" name="" id="">
+                <input wire:model="createForm.image" accept="image/*" type="file" class="mt-1" name="" id="">
+                <x-jet-input-error for="createForm.image" />
             </div>
 
         </x-slot>
 
         <x-slot name="actions">
-
+            <x-button type="submit" color="green">
+                Crear categoría
+            </x-button><x-jet-button>
+                Agregar
+            </x-jet-button>
         </x-slot>
     </x-jet-form-section>
 </div>
