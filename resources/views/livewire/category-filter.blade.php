960ea7f6f745bfc7dcdc59111fb4400f88136c03
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-02-08 20:57:37 +0100

    Revert "Fin vídeo 187"
    This reverts commit a2784d7bd92152f27c8faee0f959f84c8da43950.

--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -3,7 +3,7 @@
         <div class="px-6 py-2 flex justify-between items-center">
             <h1>{{ $category->name }}</h1>
 
-            <div class="hidden md:block grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
+            <div class="grid grid-cols-2 border border-gray-200 divide-x divide-gray-200 text-gray-500">
                 <i class="fas fa-border-all p-3 cursor-pointer {{ $view == 'grid' ? 'text-orange-500' : '' }}" wire:click="$set('view', 'grid')"></i>
                 <i class="fas fa-th-list p-3 cursor-pointer {{ $view == 'list' ? 'text-orange-500' : '' }}" wire:click="$set('view', 'list')"></i>
             </div>
@@ -44,20 +44,7 @@
                 <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-6">
                     @forelse($products as $product)
                         <li class="bg-white rounded-lg shadow">
-                            <article>
-                                <figure>
-                                    <img class="h-48 w-full object-cover object-center"
-                                         src="{{ Storage::url($product->images->first()->url) }}" alt="">
-                                </figure>
-                                <div class="py-4 px-6">
-                                    <h1 class="text-lg font-semibold">
-                                        <a href="{{ route('products.show', $product) }}">
-                                            {{ Str::limit($product->name, 20) }}
-                                        </a>
-                                    </h1>
-                                    <p class="font-bold text-trueGray-700">{{ $product->price }} &euro;</p>
-                                </div>
-                            </article>
+                            …
                         </li>
                     @empty
                         <li class="md:col-span-2 lg:col-span-4">
@@ -71,7 +58,7 @@
             @else
                 <ul>
                     @forelse($products as $product)
-                        <x-products-list :product="$product"></x-products-list>
+                        <x-product-list :product="$product"></x-product-list>
                     @empty
                         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
                             <strong class="font-bold">Upss!</strong>
