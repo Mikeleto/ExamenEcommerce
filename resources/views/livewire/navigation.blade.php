5647dd4680047ad5f8705b95b0b3bddd11e5d273
Mikeleto <miguel2015mlsol13@gmail.com>
2024-02-27 15:57:58 +0100

    Inicio examen

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -5,7 +5,7 @@
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-            <span class="text-sm hidden sm:block">
+            <span class="text-sm hidden sm:block" >
                 Categorías
             </span>
         </a>
